9634bc5d5fa2a5309ed9fdf4cac97021106c8031
Lukas Mauser <l.mauser.dev@gmail.com>
2020-10-10 15:45:44 +0200

    Style: shop results

--- components/pageElements/landingPage/ShopSearchResult.vue
+++ components/pageElements/landingPage/ShopSearchResult.vue
@@ -1,33 +1,63 @@
 <template>
-  <div class="w-1/2" :class="{ active: selected }" @click="$emit('selectShop')">
-    {{ selected }}
-    <div class="prose">
-      <div class="flex w-full">
-        <img :src="shop.images.cover" class="object-cover w-full" />
-
-        <div v-if="selected">
-          <span> {{ $t('landing_page.shop_list.open_until') }}</span>
-          <p>{{ shop.description }}</p>
+  <div
+    class="col-span-1"
+    :class="{ active: selected }"
+    @click="$emit('selectShop')"
+  >
+    <div>
+      <div class="flex w-full p-4">
+        <div class="w-1/2 relative pb-1/4">
+          <img
+            :src="shop.images.cover"
+            class="absolute top-0 left-0 h-full object-cover w-full rounded-lg"
+          />
+        </div>
+        <div v-if="selected" class="w-1/2">
+          <div class="flex">
+            <span class="ml-auto bg-primary-lighter p-1 rounded-md">
+              {{ $t('landing_page.shop_list.open_until') }}{{ ' 18 Uhr' }}</span
+            >
+          </div>
+          <p class="mt-2">{{ shop.description }}</p>
         </div>
       </div>
-      <div class="flex">
-        <div>
-          <h3>{{ shop.name }}</h3>
-          <p>{{ shop.address }}</p>
+
+      <div class="w-full flex" :class="{ bottomRow: selected }">
+        <div class="w-1/2">
+          <h3 class="my-0 text-lg font-black">{{ shop.name }}</h3>
+          <p class="text-sm">{{ shop.address }}</p>
         </div>
-        <button v-if="selected" class="secondary">
-          {{ $t('landing_page.shop_list.call') }}
-        </button>
-        <button v-if="selected" class="secondary">
-          {{ $t('landing_page.shop_list.email') }}
-        </button>
-        <button
-          v-if="selected"
-          class="primary"
-          @click="$router.push(`/shops/${shop.slug}`)"
-        >
-          {{ $t('landing_page.shop_list.go_to_store') }}
-        </button>
+        <div class="flex w-1/2 items-center">
+          <div class="flex-auto">
+            <button v-if="selected" class="p-0">
+              <div>
+                <eva-icon class="mx-auto" name="phone-outline" />
+              </div>
+              <div class="text-sm">
+                {{ $t('landing_page.shop_list.call') }}
+              </div>
+            </button>
+          </div>
+          <div class="flex-auto">
+            <button v-if="selected" class="p-0">
+              <div>
+                <eva-icon class="mx-auto" name="email-outline" />
+              </div>
+              <div class="text-sm">
+                {{ $t('landing_page.shop_list.email') }}
+              </div>
+            </button>
+          </div>
+          <div class="flex-auto">
+            <button
+              v-if="selected"
+              class="bg-primary text-white w-full"
+              @click="$router.push(`/shops/${shop.slug}`)"
+            >
+              {{ $t('landing_page.shop_list.go_to_store') }}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -62,6 +92,14 @@ export default {
 
 <style lang="scss" scoped>
 .active {
-  @apply col-span-2;
+  @apply col-span-2 border border-grey-light rounded-lg rounded-b-lg;
+}
+
+.bottomRow {
+  @apply bg-primary-lightest rounded-b-lg px-4;
+}
+
+.small-heading {
+  @apply m-0;
 }
 </style>
