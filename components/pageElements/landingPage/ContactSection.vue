1d43f73d290c967c371c30625bbb1fbd5ef2d270
Lukas Mauser <l.mauser.dev@gmail.com>
2020-10-30 10:22:27 +0100

    Feat: newsletter

--- components/pageElements/landingPage/ContactSection.vue
+++ components/pageElements/landingPage/ContactSection.vue
@@ -3,6 +3,15 @@
     <div class="container prose text-center py-10 px-2">
       <h2>{{ $t('landing_page.contact.title') }}</h2>
       <p>{{ $t('landing_page.contact.text') }}</p>
+      <a
+        href="https://lokabees.us17.list-manage.com/subscribe?u=1d9f91ed93a76745bb2f66d4e&id=546725ca15"
+        target="_blank"
+      >
+        <button class="primary w-full">
+          {{ $t('landing_page.contact.button') }}
+        </button>
+      </a>
+      <!--
       <FormulateForm @submit="submit">
         <div class="flex w-full justify-center">
           <div class="p-1">
@@ -20,7 +29,7 @@
           </div>
         </div>
       </FormulateForm>
-    </div>
+    --></div>
   </div>
 </template>
 
