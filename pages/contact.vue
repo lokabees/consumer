c8ccf8b253501da9ff1be38953b1be5927780057
Lukas Mauser <l.mauser.dev@gmail.com>
2020-11-07 10:41:37 +0100

    Feat: contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,44 @@
 <template>
-  <div>contact</div>
+  <div class="container max-w-2xl py-8">
+    <h1>{{ $t('contact.title') }}</h1>
+    <FormulateForm v-model="contact" @submit="submit">
+      <FormulateInput
+        name="name"
+        type="text"
+        :placeholder="$t('contact.name_placeholder')"
+        :label="$t('contact.name_label')"
+        validation="required"
+      />
+      <FormulateInput
+        name="email"
+        type="text"
+        :placeholder="$t('contact.email_placeholder')"
+        :label="$t('contact.email_label')"
+        validation="required|email"
+      />
+      <FormulateInput
+        name="text"
+        type="textarea"
+        :placeholder="$t('contact.text_placeholder')"
+        :label="$t('contact.text_label')"
+        validation="required"
+      />
+      <FormulateInput type="submit" :label="$t('contact.submit_label')" />
+    </FormulateForm>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      contact: {},
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.contact)
+    },
+  },
+}
 </script>
